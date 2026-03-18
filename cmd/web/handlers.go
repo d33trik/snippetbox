522834c7b61d37c2d480129fed0ad065fa7f2ca5
Claudio Dietrich <d33trik@gmail.com>
2026-03-18 17:03:50 -0300

    Display latest snippets on the home page
    Update the home handler to retrieve the 10 most recent snippets from the
    database and pass them to the template.
    
    This replaces the static placeholder text with a dynamic table that
    displays snippet titles, creation dates, and IDs, providing users with
    immediate access to recent content.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,6 +11,12 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
 	files := []string{
 		"./ui/html/base.html",
 		"./ui/html/partials/nav.html",
@@ -23,7 +29,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
+	data := templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
