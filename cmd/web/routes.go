0b45d151d75982d96d942d7ef41fe0a0dfee10be
Claudio Dietrich <d33trik@gmail.com>
2026-04-09 16:23:40 -0300

    Add user authentication handlers and routes
    Add placeholder handler functions and routes for user signup, login,
    logout, and related forms for user authentication.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,9 +15,16 @@ func (app *application) routes() http.Handler {
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	mux.Handle("POST /snippet/save", dynamic.ThenFunc(app.snippetSave))
+
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/save", dynamic.ThenFunc(app.userSave))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/authentcate", dynamic.ThenFunc(app.userAuthenticate))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogout))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 
