ab983ce8fa729bde0627c6c863a945fb0365a9d6
Claudio Dietrich <d33trik@gmail.com>
2026-04-15 14:51:34 -0300

    Add E2E test for healthcheck endpoint

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,6 +22,36 @@ func TestHealthCheck(t *testing.T) {
 	healthCheck(rr, req)
 
 	res := rr.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, res.StatusCode, http.StatusOK)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
+
+func TestHealthCheckE2E(t *testing.T) {
+	app := &application{
+		logger: slog.New(slog.DiscardHandler),
+	}
+
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	req, err := http.NewRequest(http.MethodGet, ts.URL+"/healthcheck", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer res.Body.Close()
 
 	assert.Equal(t, res.StatusCode, http.StatusOK)
