144c6ea88ed94230aa67a607e78809c633a87c6d
Claudio Dietrich <d33trik@gmail.com>
2026-04-14 21:11:30 -0300

    Add health check endpoint and test

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"codeberg.org/d33trik/snippetbox/internal/assert"
+)
+
+func TestHealthCheck(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	healthCheck(rr, req)
+
+	res := rr.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, res.StatusCode, http.StatusOK)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
